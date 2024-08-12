d543d76251fb4de20e72f869ddcfdde6e5e07205
AbdulSamad18090 <abdulsamad18090@gmail.com>
2024-08-12 17:09:33 +0500

    set up content area

--- app/page.js
+++ app/page.js
@@ -88,95 +88,100 @@ function App() {
   };
 
   return (
-    <div className="flex bg-[#1e1f20] h-screen text-white py-4">
-      {/* Sidebar */}
-      <div className={`sidebar ${isOpen ? "open" : "closed "}`}>
+    <div className="bg-[#1e1f20] h-screen">
+      {/* Header */}
+      <div className="w-full text-white p-4">
         <IoMdMenu
           className="text-3xl cursor-pointer"
           onClick={() => {
             setIsOpen(!isOpen);
           }}
         />
-        <div className="my-10">
-          <button className="flex gap-2 items-center bg-white bg-opacity-10 hover:bg-opacity-20 p-2 rounded-lg transition-all">
-            <FaPlus className="text-lg" />
-            <p className={`new-chat text-[12px] ${isOpen ? "" : "hidden"}`}>
-              New Chat
-            </p>
-          </button>
-        </div>
-        {isOpen && (
-          <div>
-            <h1 className="font-semibold">Recents</h1>
-            <div className="h-[350px] overflow-auto">
-              {/* Placeholder content */}
-              {Array.from({ length: 25 }, (_, i) => (
-                <h1 key={i}>as</h1>
-              ))}
-            </div>
-          </div>
-        )}
       </div>
-      {/* Content */}
-      <div className="bg-[#131314] w-full rounded-l-2xl flex flex-col justify-between p-[2%]">
-        <div className="flex flex-col gap-4 overflow-y-auto">
-          {chat.length === 0 ? (
-            <>
-              <h1 className="md:text-6xl text-5xl gradient-text font-semibold w-fit">
-                Hello, User
-              </h1>
-              <p className="md:text-5xl text-4xl text-[#444746] font-semibold">
-                How can I help you today?
+      <div className="flex bg-[#1e1f20] text-white">
+        {/* Sidebar */}
+        <div className={`sidebar ${isOpen ? "open" : "closed "}`}>
+          <div className="my-10">
+            <button className="flex gap-2 items-center bg-white bg-opacity-10 hover:bg-opacity-20 p-2 rounded-lg transition-all">
+              <FaPlus className="text-lg" />
+              <p className={`new-chat text-[12px] ${isOpen ? "" : "hidden"}`}>
+                New Chat
               </p>
-              <div className="flex flex-wrap items-center justify-center m-[3%] gap-4 text-[13px] font-thin">
-                {defaultPrompts.map((defaultPrompt, i) => (
-                  <div
-                    key={i}
-                    className="flex flex-col justify-between items-end bg-[#1e1f20] p-3 rounded-xl min-h-[180px] max-w-[180px] hover:bg-[#272727] cursor-pointer transition-all hover:rotate-3 hover:scale-110"
-                    onClick={() => {
-                      setPrompt(defaultPrompt.text);
-                    }}
-                  >
-                    <h1>{defaultPrompt.text}</h1>
-                    <div className="bg-black bg-opacity-30 text-xl p-2 rounded-full">
-                      {defaultPrompt.icon}
-                    </div>
-                  </div>
+            </button>
+          </div>
+          {isOpen && (
+            <div>
+              <h1 className="font-semibold">Recents</h1>
+              <div className="h-[350px] overflow-auto">
+                {/* Placeholder content */}
+                {Array.from({ length: 25 }, (_, i) => (
+                  <h1 key={i}>as</h1>
                 ))}
               </div>
-            </>
-          ) : (
-            <div>
-              {chat.map((entry, index) => (
-                <div key={index} className="mb-4 px-2 md:mx-[10%]">
-                  <h2 className="text-lg bg-[#1e1f20] py-2 px-4 rounded-lg md:min-w-[60%] min-w-[90%]  md:max-w-[60%] max-w-[90%] mr-0 ml-auto my-4">
-                    {entry.prompt}
-                  </h2>
-                  <p className="text-md">{entry.response}</p>
-                </div>
-              ))}
             </div>
           )}
         </div>
+        {/* Content */}
+        <div className="bg-[#131314] w-full h-[85vh] items-stretch rounded-l-2xl flex flex-col justify-between p-2">
+          <div className="flex flex-col gap-4 overflow-y-auto">
+            {chat.length === 0 ? (
+              <>
+                <h1 className="md:text-6xl text-5xl gradient-text font-semibold w-fit">
+                  Hello, User
+                </h1>
+                <p className="md:text-5xl text-4xl text-[#444746] font-semibold">
+                  How can I help you today?
+                </p>
+                <div className="p-4 flex flex-wrap items-center justify-center gap-4 text-[13px] font-thin w-full overflow-x-auto">
+                  {defaultPrompts.map((defaultPrompt, i) => (
+                    <div
+                      key={i}
+                      className="flex flex-col justify-between items-end bg-[#1e1f20] p-3 rounded-xl min-h-[180px] max-w-[180px] hover:bg-[#272727] cursor-pointer transition-all hover:rotate-3 hover:scale-110"
+                      onClick={() => {
+                        setPrompt(defaultPrompt.text);
+                      }}
+                    >
+                      <h1>{defaultPrompt.text}</h1>
+                      <div className="bg-black bg-opacity-30 text-xl p-2 rounded-full">
+                        {defaultPrompt.icon}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </>
+            ) : (
+              <div>
+                {chat.map((entry, index) => (
+                  <div key={index} className="mb-4 px-2 md:mx-[10%]">
+                    <h2 className="text-lg bg-[#1e1f20] py-2 px-4 rounded-lg md:min-w-[60%] min-w-[90%]  md:max-w-[60%] max-w-[90%] mr-0 ml-auto my-4">
+                      {entry.prompt}
+                    </h2>
+                    <p className="text-md">{entry.response}</p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
 
-        <form
-          onSubmit={generateAnswer}
-          className="flex items-center gap-4 w-full bg-[#1e1f20] py-4 px-8 mt-[3%] rounded-full max-h-24"
-        >
-          <input
-            type="text"
-            placeholder="Enter a prompt here"
-            value={prompt}
-            className="w-full bg-transparent ring-0 outline-none"
-            onChange={(e) => {
-              setPrompt(e.target.value);
-            }}
-            disabled={loading}
-          />
-          <button type="submit" className="text-2xl" disabled={loading}>
-            <IoSend />
-          </button>
-        </form>
+          <form
+            onSubmit={generateAnswer}
+            className="flex items-center gap-4 w-full bg-[#1e1f20] py-4 px-8 rounded-full max-h-24"
+          >
+            <input
+              type="text"
+              placeholder="Enter a prompt here"
+              value={prompt}
+              className="w-full bg-transparent ring-0 outline-none"
+              onChange={(e) => {
+                setPrompt(e.target.value);
+              }}
+              disabled={loading}
+            />
+            <button type="submit" className="text-2xl" disabled={loading}>
+              <IoSend />
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
